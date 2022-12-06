72fe385439c47663f4412f2eba72674a9c6a424c
Menna11 <mennaaymann11@gmail.com>
2022-12-06 23:19:05 +0200

    HomePageOutline

---
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.bookstore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Button homeBtn= findViewById(R.id.homeBtn);
+        Button categoryBtn= findViewById(R.id.categoryBtn);
+        Button cartBtn= findViewById(R.id.cartBtn);
+
+        homeBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        categoryBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Category.class);
+                startActivity(i);
+            }
+        });
+
+        cartBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i=new Intent(MainActivity.this,Cart.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
